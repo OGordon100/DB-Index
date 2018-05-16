5b355fef4456f8917abbf4fb93339f8a2ad88b22
OGordon100 <oliverg123@ntlworld.com>
2018-05-16 22:04:30 +0100

    Get full batting/bowling scorecards

--- PlayerSelector.py
+++ PlayerSelector.py
@@ -1,9 +1,11 @@
+import np as np
 from espncricinfo.match import Match
 import random
 import urllib.request
 import re
 import numpy as np
 from bs4 import BeautifulSoup
+import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as pyplot
 
@@ -29,17 +31,19 @@ NationNumsAll = {'england': 1,
                  'afghanistan': 40}
 
 PlayerNameAll = {}
+BatsmanText = np.empty(0)
+
 # Select a format & team
 while True:
     try:
         PlayerTeamName = input('Enter a test playing nation: ').lower()
-        PlayerTeamNum = NationNumsAll[PlayerTeamName]
+        PlayerTeamID = NationNumsAll[PlayerTeamName]
         break
     except (NameError, KeyError):
         print("Team not found! ", end="", flush=True)
 
 # Parse and extract list of all players for nation
-PlayerListURL = f"http://www.espncricinfo.com/england/content/player/caps.json?country={PlayerTeamNum!s};class=1"
+PlayerListURL = f"http://www.espncricinfo.com/england/content/player/caps.json?country={PlayerTeamID!s};class=1"
 PlayerListJSON = urllib.request.urlopen(PlayerListURL).read()
 PlayerListSoup = BeautifulSoup(PlayerListJSON, "lxml")
 PlayerListSoupLi = PlayerListSoup.find_all("li", {"class": "ciPlayername"})
@@ -50,8 +54,8 @@ for PConstructor in PlayerListSoupLi:
     PConstructNamePos = PConstructString.find("middle;") + 9
 
     PConstructName = PConstructString[PConstructNamePos:len(PConstructString) - 9]
-    PConstructNum = int(re.findall('\\d+', PConstructURL)[0])
-    PlayerNameAll.update({PConstructName: PConstructNum})
+    PConstructID = int(re.findall("\\d+", PConstructURL)[0])
+    PlayerNameAll.update({PConstructName: PConstructID})
 
 # Pick player
 print(f"Found {len(PlayerNameAll)!s} players for {PlayerTeamName.title()!s}")
@@ -67,3 +71,32 @@ while True:
         except KeyError:
             print("Player not found! ", end="", flush=True)
             continue
+
+# Get all matches player has played in
+AllMatchURL = (f'http://stats.espncricinfo.com/ci/engine/player/{PlayerNum!s}'
+               '.json?class=1;template=results;type=allround;view=match')
+AllMatchJSON = urllib.request.urlopen(AllMatchURL).read()
+AllMatchSoup = BeautifulSoup(AllMatchJSON, "lxml")
+AllMatchTable = AllMatchSoup.findAll("table", {"class": "engineTable"})[3]
+MatchIDAll = re.findall('\\d+', str(AllMatchTable.findAll("td", {"style": "white-space: nowrap;"})))[::2]
+
+# Get scorecard of each match played in
+print(f"Found {(len(MatchIDAll))!s} matches for {PlayerName}. Analysing...")
+for MatchID in MatchIDAll:
+
+    print('Match')
+
+    # Open URL
+    MatchURL = f"http://www.espncricinfo.com/ci/engine/match/{MatchID!s}.html"
+    MatchJSON = urllib.request.urlopen(MatchURL).read()
+    MatchSoup = BeautifulSoup(MatchJSON, "lxml")
+
+    # Get bowling table (this is how the internet is meant to be programmed)
+    BowlingScorecard = pd.read_html(MatchURL)
+
+    # Get batting table (this is how the internet is NOT meant to be programmed...)
+    AllBatsmanRawText = MatchSoup.findAll("div", {"class": "cell runs"})
+    AllBatsmanRawTextNames = MatchSoup.findAll("div", {"class": "cell batsmen"})
+    BatsmanText = np.array([BatsmanRawText.text for BatsmanRawText in AllBatsmanRawText]).reshape((-1, 6))
+    BatsmanNames = np.vstack(np.array([BatsmanRawTextNames.text for BatsmanRawTextNames in AllBatsmanRawTextNames]))
+    BattingScorecard = np.concatenate((BatsmanNames,BatsmanText),1)
